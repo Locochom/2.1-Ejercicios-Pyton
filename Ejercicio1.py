420e1dc848e248c86fdde222111a52b59e8baa2d
Locochom <126177808+Locochom@users.noreply.github.com>
2025-03-13 16:20:05 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+# Solicitar los datos al usuario
+nombre = input("Ingrese su nombre completo: ")
+semestre = input("Ingrese su semestre: ")
+grupo = input("Ingrese su grupo: ")
+carrera = input("Ingrese su carrera: ")
+
+# Mostrar los datos en pantalla
+print("\nInformación del estudiante:")
+print(f"Nombre: {nombre}")
+print(f"Semestre: {semestre}")
+print(f"Grupo: {grupo}")
+print(f"Carrera: {carrera}")
